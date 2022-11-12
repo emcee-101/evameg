a45e4209167b09ce35ebab885490705e63762585
niklash <nik.las.herzog@t-online.de>
2022-11-12 16:07:49 +0100

    work on ability to invoke SetupActivity on first launsch

--- app/src/main/java/de/egovt/evameg/MainActivity.kt
+++ app/src/main/java/de/egovt/evameg/MainActivity.kt
@@ -1,11 +1,31 @@
 package de.egovt.evameg
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
+import androidx.appcompat.app.AppCompatActivity
+import de.egovt.evameg.SettingsStorage
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sets:SettingsStorage = SettingsStorage();
+        sets.KeyValueStore(application);
+        val notFirstStart:Boolean = sets.getBool("notFirstStart");
+
+        // if the app is started for the first time
+        // double negation to allow for a negative default value (ask Niklas, if you dont know what that means)
+        if (!notFirstStart) {
+                // REMEMBER TO SET THE VALUE TO POSITIVE AFTER THAT
+                // store.writeIntValue(STORE_KEY_COUNTER, 42);
+
+            // INTENT -> SetupActivity
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/de/egovt/evameg/SettingsStorage.kt
@@ -0,0 +1,35 @@
+package de.egovt.evameg
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+
+
+class SettingsStorage {
+
+    private val KEY_VALUE_STORE_FILE_NAME = "settings_kv"
+
+    private var app: Application? = null
+
+    public fun KeyValueStore(application: Application) {
+        this.app = application;
+    }
+
+    private fun  getPreferences(): SharedPreferences? {
+        return this.app?.getSharedPreferences(KEY_VALUE_STORE_FILE_NAME, Context.MODE_PRIVATE)
+    }
+
+    public fun writeBool(key: String, value: Boolean){
+
+        this.getPreferences()!!.edit().putBoolean(key, value).apply();
+
+
+    }
+
+    public fun getBool(key: String): Boolean {
+
+        return this.getPreferences()!!.getBoolean(key, false)
+
+    }
+
+}
