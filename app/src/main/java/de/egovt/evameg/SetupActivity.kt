cef8dd1193dac3b4a0097728f3f5310c847d2ae1
niklash <nik.las.herzog@t-online.de>
2022-11-12 15:31:39 +0100

    fix on initial welcome

--- app/src/main/java/de/egovt/evameg/SetupActivity.kt
+++ app/src/main/java/de/egovt/evameg/SetupActivity.kt
@@ -3,13 +3,10 @@ package de.egovt.evameg
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SetupActivity {
 
-    class MainActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_setup)
-        }
+class SetupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup)
     }
-
 }
