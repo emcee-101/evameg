526c2241beddb1c6d9a2d2373401881ec736ea87
niklash <nik.las.herzog@t-online.de>
2022-11-12 15:29:50 +0100

    initial welcome screen

---
+++ app/src/main/java/de/egovt/evameg/SetupActivity.kt
@@ -0,0 +1,15 @@
+package de.egovt.evameg
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SetupActivity {
+
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_setup)
+        }
+    }
+
+}
