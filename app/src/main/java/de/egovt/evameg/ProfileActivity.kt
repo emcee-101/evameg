9ba312df27098ba3a4f9faf9e8aa1bd1f6ea131f
ce7248lu <celina.ludwigs@fh-erfurt.de>
2022-11-13 21:17:45 +0100

    Profile in work (bottom navigation deleted, unnecessary files deleted,profile imageView changed)

--- app/src/main/java/de/egovt/evameg/ProfileActivity.kt
+++ app/src/main/java/de/egovt/evameg/ProfileActivity.kt
@@ -17,19 +17,6 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityProfileBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding.root)}
 
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_profile)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
 }
