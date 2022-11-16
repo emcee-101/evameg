65bc49d868f17de76f02fb7bf169afe0a8701a57
ce7248lu <celina.ludwigs@fh-erfurt.de>
2022-11-16 17:21:23 +0100

    Layout dialog popup revised, so every field is visible in data edit mode

--- app/src/main/java/de/egovt/evameg/ProfileActivity.kt
+++ app/src/main/java/de/egovt/evameg/ProfileActivity.kt
@@ -1,11 +1,13 @@
 package de.egovt.evameg
 
 import android.app.AlertDialog
+import android.content.Context
 
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
