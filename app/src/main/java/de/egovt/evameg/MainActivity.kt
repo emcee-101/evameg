5591858e94c3128810bad502cd83196357b46fa6
DEVmozi <zid.m@pm.me>
2022-11-20 15:36:04 +0100

    init commit, empty project

--- app/src/main/java/de/egovt/evameg/MainActivity.kt
+++ app/src/main/java/de/egovt/evameg/MainActivity.kt
@@ -1,11 +1,19 @@
 package de.egovt.evameg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val newApplication = findViewById<FloatingActionButton>(R.id.new_application)
+        newApplication.setOnClickListener {
+            val Intent = Intent(this, NewApplication::class.java)
+            startActivity(Intent)
+        }
+
     }
 }
