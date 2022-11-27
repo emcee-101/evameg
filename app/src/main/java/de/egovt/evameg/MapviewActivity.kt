99c445e7c84d3575f2240ce7aa0f42e274194b2b
niklash <nik.las.herzog@t-online.de>
2022-11-27 13:50:39 +0100

    work on map - added todos and more

--- app/src/main/java/de/egovt/evameg/MapviewActivity.kt
+++ app/src/main/java/de/egovt/evameg/MapviewActivity.kt
@@ -1,21 +1,49 @@
 package de.egovt.evameg
 
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import de.egovt.evameg.databinding.ActivityMapviewBinding
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
-import de.egovt.evameg.databinding.ActivityMapviewBinding
+
 
 class MapviewActivity  : AppCompatActivity() {
 
     lateinit var mMap : MapView
 
+    fun checkForPermissions( code : Manifest.permission){
+
+        if (ContextCompat.checkSelfPermission(MapviewActivity.class, code) == PackageManager.PERMISSION_DENIED) {
+            // Requesting the permission
+            ActivityCompat.requestPermissions(MapviewActivity.class, arrayOf(code), 0L)
+        } else {
+            Toast.makeText(MapviewActivity.class, "Permission already granted", Toast.LENGTH_SHORT).show()
+        }
+
+
+        if ((checkSelfPermission(code) == PackageManager.PERMISSION_GRANTED)) {
+            Log.v("TAG", "Permission there yessss")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // TODO ask for permissions more gracefully
+        // TODO read about databinding, even though it shouldnt be needed here
+        // TODO make this thing wórk
+
+
+
         Configuration.getInstance().userAgentValue = packageName
 
         val binding = ActivityMapviewBinding.inflate(layoutInflater)
@@ -23,10 +51,6 @@ class MapviewActivity  : AppCompatActivity() {
 
         mMap = binding.coolMap
         mMap.setTileSource(TileSourceFactory.MAPNIK)
-
-
-
-
 
 
         val longitude = intent.getDoubleExtra("longitude", 36.7783)
