cd8416a286a11ab8f40bbf42328d22f3a6ac03d2
niklash <nik.las.herzog@t-online.de>
2022-11-27 12:54:01 +0100

    work on map - another try

--- app/src/main/java/de/egovt/evameg/MapviewActivity.kt
+++ app/src/main/java/de/egovt/evameg/MapviewActivity.kt
@@ -1,63 +1,47 @@
 package de.egovt.evameg
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
+
 import android.os.Bundle
-import android.util.Log
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import org.osmdroid.api.IMapController
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
-
+import de.egovt.evameg.databinding.ActivityMapviewBinding
 
 class MapviewActivity  : AppCompatActivity() {
-    private var map: MapView? = null
-    private var mapController: IMapController? = null
 
+    lateinit var mMap : MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        if (Build.VERSION.SDK_INT >= 23) {
-            if ((checkSelfPermission(android.Manifest.permission.ACCESS_NETWORK_STATE) == PackageManager.PERMISSION_GRANTED) && (checkSelfPermission(android.Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) && (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) && (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
-                Log.v("TAG", "Permission there yessss")
-            } else {
-                Log.v("TAG", "Permission is revoked");
-                ActivityCompat.requestPermissions(this,
-                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE as String), 1);
-            }
-        }
-
-
-        //Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
         Configuration.getInstance().userAgentValue = packageName
 
+        val binding = ActivityMapviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mMap = binding.coolMap
+        mMap.setTileSource(TileSourceFactory.MAPNIK)
 
 
-        //inflate and create the map
-        setContentView(R.layout.activity_mapview)
-        map = findViewById<MapView>(R.id.coolMap)
 
-        map?.setTileSource(TileSourceFactory.MAPNIK)
 
-        map?.setMultiTouchControls(true)
-        mapController = map?.controller
+
+
+        val longitude = intent.getDoubleExtra("longitude", 36.7783)
+        val latitude = intent.getDoubleExtra("latitude", 119.4179)
+
+
+        val controller = mMap.controller
+
+        val mapPoint = GeoPoint(latitude, longitude)
+
+        controller.setZoom(9.5)
+
+        controller.animateTo(mapPoint)
+
     }
 
-
-    override fun onResume() {
-        super.onResume()
-
-        map?.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        map?.onPause()
-    }
 
 }
