4e54dcc1c3b90a04479d3121fed19b8dc67d7718
DEVmozi <zid.m@pm.me>
2022-12-11 16:03:42 +0100

    WIP: initial dynamic form layout

--- app/src/main/java/de/egovt/evameg/FormActivity.kt
+++ app/src/main/java/de/egovt/evameg/FormActivity.kt
@@ -2,10 +2,29 @@ package de.egovt.evameg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
+
 
 class FormActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+
+    }
+    private fun addItem(hint : String){
+        val editLinearLayout = findViewById<LinearLayout>(R.id.container)
+        val view = findViewById<LinearLayout>(R.id.dynamic_edit_text)
+        val editText = EditText(this)
+        editText.layoutParams = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT)
+        editText.setPadding(20, 20, 20, 20)
+        editText.hint = hint
+        editLinearLayout?.addView(editText)
+        view.addView(editText)
+
     }
 }
