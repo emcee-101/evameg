3af4d0feed13f96ed5b2eb801254f22567227892
Niklas Herzog <nik.las.herzog@t-online.de>
2023-02-01 22:01:25 +0100

    add overflow button to MainActivity.kt and enable Preferences

--- app/src/main/java/de/egovt/evameg/MainActivity.kt
+++ app/src/main/java/de/egovt/evameg/MainActivity.kt
@@ -4,18 +4,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import de.egovt.evameg.Fragments.Home
 import de.egovt.evameg.activities.SetupActivity
 import de.egovt.evameg.activities.test_activity
 import de.egovt.evameg.databinding.ActivityMainBinding
 import de.egovt.evameg.fragments.MapViewFragment
+import de.egovt.evameg.fragments.PreferencesFragment
 import de.egovt.evameg.fragments.Profile
 import de.egovt.evameg.utility.SettingsStorage
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.options_overflow, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.settings_panel_overflow -> {
+
+                Log.i("aaaa", "Settings called")
+                replaceFragment(PreferencesFragment() as Fragment)
+                true
+            }
+            else -> {Log.d("helphelphelp", "Unknown Option registered"); false}
+        }
+    }
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
